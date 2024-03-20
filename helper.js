49a75ff662d72dd835c7c77c31e7ff53d150fff1
Richard Shaju <richardshaju66@gmail.com>
2024-03-20 19:53:05 +0530

    Helper file is created to improve the codebase

--- helper.js
+++ helper.js
@@ -1,8 +1,68 @@
-export async function cityWeather (city){
-        const city = req.params.city;
-        const weatherResponse = await axios.get(
-          `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
-        );
-        const weatherData = weatherResponse.data;
-        return weatherData
-}
+const axios = require('axios');
+require('dotenv').config();
+
+
+module.exports = {
+  cityWeather: async (city) => {
+    try {
+      const weatherResponse = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
+      );
+      const weatherData = weatherResponse.data;
+      return weatherData;
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      throw error;
+    }
+  },
+  userWeather: async (coordinates) => {
+    const weatherData = {
+      weather: {},
+      details: {},
+    };
+    try {
+      const weatherResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=551ef908161a9cc4b399b037d5e00cef`
+      );
+  
+      // Rearranging data from api to our needs
+      weatherData.weather.main = weatherResponse.data.weather[0].main;
+      weatherData.weather.description = weatherResponse.data.weather[0].description;
+      weatherData.weather.temp = weatherResponse.data.main.temp;
+      weatherData.weather.feels_like = weatherResponse.data.main.feels_like;
+      weatherData.weather.temp_min = weatherResponse.data.main.temp_min;
+      weatherData.weather.temp_max = weatherResponse.data.main.temp_max;
+      weatherData.weather.pressure = weatherResponse.data.main.pressure;
+      weatherData.weather.humidity = weatherResponse.data.main.humidity;
+      weatherData.weather.visibility = weatherResponse.data.main.visibility;
+      weatherData.weather.wind_speed = weatherResponse.data.main.wind_speed;
+  
+      weatherData.details.name = weatherResponse.data.name;
+      weatherData.details.country = weatherResponse.data.sys.country;
+      weatherData.details.timezone = weatherResponse.data.timezone;
+  
+      return weatherData
+    } catch (error) {
+      throw error;
+    }
+  },
+  nearByPlaces: async(coordinates) => {
+    try {
+      const query = `[out:json];(
+          node(around:1000,${coordinates.latitude},${coordinates.longitude})["amenity"="restaurant"];
+          node(around:1000,${coordinates.latitude},${coordinates.longitude})["leisure"="park"];
+          node(around:1000,${coordinates.latitude},${coordinates.longitude})["tourism"];
+        );out;`;
+        const response = await axios.get(`https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`);
+        const nearbyPlaces = response.data.elements.map(place => ({
+          id: place.id,
+          type: place.tags.amenity || place.tags.leisure || place.tags.tourism,
+          name: place.tags.name || place.tags.tourism,
+          coordinates: [place.lon, place.lat]
+        }));
+   return nearbyPlaces
+  } catch (error) {
+    throw(error)
+  }
+  }
+};
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const { cityWeather } = require("./helper");
+const helper = require('./helper.js')
 
 // Initialize Express app
 const app = express();
@@ -33,7 +33,7 @@ const User = mongoose.model("User", userSchema);
 // API endpoint for fetching weather data
 app.get("/weather/:city", async (req, res) => {
   try {
-    const result = cityWeather(req.params.city)
+    const result = await helper.cityWeather(req.params.city)
     res.json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -42,71 +42,24 @@ app.get("/weather/:city", async (req, res) => {
 
 app.get("/user/:lat&:log", async (req, res) => {
   // Lattitude and logitude of the user's position is passed from the frontend
-  const weatherData = {
-    weather: {},
-    details: {},
-  };
+  const coordinates = {
+    latitude : req.params.lat,
+    longitude : req.params.log,
+  }
+
   try {
-    const latitude = req.params.lat;
-    const longitude = req.params.log;
-    const weatherResponse = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=551ef908161a9cc4b399b037d5e00cef`
-    );
+    const weather = await helper.userWeather(coordinates)
+    const nearby = await helper.nearByPlaces(coordinates)
 
-    // Rearranging data from api to our needs
-    weatherData.weather.main = weatherResponse.data.weather[0].main;
-    weatherData.weather.description =
-      weatherResponse.data.weather[0].description;
-    weatherData.weather.temp = weatherResponse.data.main.temp;
-    weatherData.weather.feels_like = weatherResponse.data.main.feels_like;
-    weatherData.weather.temp_min = weatherResponse.data.main.temp_min;
-    weatherData.weather.temp_max = weatherResponse.data.main.temp_max;
-    weatherData.weather.pressure = weatherResponse.data.main.pressure;
-    weatherData.weather.humidity = weatherResponse.data.main.humidity;
-    weatherData.weather.visibility = weatherResponse.data.main.visibility;
-    weatherData.weather.wind_speed = weatherResponse.data.main.wind_speed;
-
-    weatherData.details.name = weatherResponse.data.name;
-    weatherData.details.country = weatherResponse.data.sys.country;
-    weatherData.details.timezone = weatherResponse.data.timezone;
-
-
-    
-    res.json(weatherData);
+    const data = {
+      user_weather: weather,
+      nearby: nearby
+    }
+    console.log(data);
+    res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-});
-
-
-app.get('/nearby/:lat&:log', async (req, res) => {
-    const latitude = req.params.lat;
-    const longitude = req.params.log;
-    try {
-        const query = `[out:json];(
-            node(around:1000,${latitude},${longitude})["amenity"="restaurant"];
-            node(around:1000,${latitude},${longitude})["leisure"="park"];
-            node(around:1000,${latitude},${longitude})["tourism"];
-          );out;`;
-          const response = await axios.get(`https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`);
-          const nearbyPlaces = response.data.elements.map(place => ({
-            id: place.id,
-            type: place.tags.amenity || place.tags.leisure || place.tags.tourism,
-            name: place.tags.name || place.tags.tourism,
-            coordinates: [place.lon, place.lat]
-          }));
-      res.json(nearbyPlaces);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-
-
-app.get("/", async (req, res) => {
-  const data = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=10.240298&lon=76.263130&appid=${process.env.PORT}`
-  );
-  res.send(data.data.main);
 });
 
 
